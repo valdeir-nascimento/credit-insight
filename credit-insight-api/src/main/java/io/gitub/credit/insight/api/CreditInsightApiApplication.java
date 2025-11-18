81dc75aeba9f776ee52e961c9ba7d3157387984f
Valdeir Nascimento <valdeirnascimento.dev@gmail.com>
2025-11-18 15:29:26 -0300

    first commit

---
+++ credit-insight-api/src/main/java/io/gitub/credit/insight/api/CreditInsightApiApplication.java
@@ -0,0 +1,13 @@
+package io.gitub.credit.insight.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditInsightApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditInsightApiApplication.class, args);
+	}
+
+}
---
+++ credit-insight-api/src/test/java/io/gitub/credit/insight/api/CreditInsightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitub.credit.insight.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditInsightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
