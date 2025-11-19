9f7509c7a5ef844d0b9472c9cf6fcf09ad3758cb
Valdeir Nascimento <valdeirnascimento.dev@gmail.com>
2025-11-18 21:10:38 -0300

    feature: persistence configuration

---
+++ credit-insight-api/src/main/java/io/github/credit/insight/api/infrastructure/config/ObjectMapperConfig.java
@@ -0,0 +1,17 @@
+package io.github.credit.insight.api.infrastructure.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.credit.insight.api.infrastructure.config.json.Json;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return Json.mapper();
+    }
+
+}
---
+++ credit-insight-api/src/main/java/io/github/credit/insight/api/infrastructure/config/WebServerConfig.java
@@ -0,0 +1,10 @@
+package io.github.credit.insight.api.infrastructure.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("io.github.credit.insight.api")
+public class WebServerConfig {
+
+}
